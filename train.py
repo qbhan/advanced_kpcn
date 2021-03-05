ba52853ae63baa7b59137f2e71c3e8dc9c1efa30
qbhan <smarthkb98@gmail.com>
2021-03-05 11:09:29 +0000

    feat: Runs training independent to main
    <description>
    Modulated the training method for simplicity.
    Added various arguments for further training convenience.
    
    <issue-number>

--- train.py
+++ train.py
@@ -4,27 +4,80 @@ import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as transforms
 import torch.optim as optim
+from tensorboardX import SummaryWriter
 
 import matplotlib.pyplot as plt
-# from torch.utils.tensorboard import SummaryWriter
-from tensorboardX import SummaryWriter
+import os
 import numpy as np
+import argparse
+import csv
 
 from utils import *
 from model import *
 from visualize import *
+from dataset import *
 
-L = 9 # number of convolutional layers
-n_kernels = 100 # number of kernels in each layer
-kernel_size = 5 # size of kernel (square)
+# L = 9 # number of convolutional layers
+# n_kernels = 100 # number of kernels in each layer
+# kernel_size = 5 # size of kernel (square)
 
-# input_channels = dataset[0]['X_diff'].shape[-1]
-hidden_channels = 100
+# # input_channels = dataset[0]['X_diff'].shape[-1]
+# hidden_channels = 100
 
 permutation = [0, 3, 1, 2]
-eps = 0.00316
+# eps = 0.00316
 
-def validation(diffuseNet, specularNet, dataloader, criterion, device, mode='KPCN'):
+parser = argparse.ArgumentParser(description='Train the model')
+
+'''
+Needed parameters
+1. Data & Model specifications
+device : which device will the data & model should be loaded
+mode : which kind of model should it train
+input_channel : input channel
+hidden_channel : hidden channel
+num_layer : number of layers / depth of models
+'''
+parser.add_argument('--device', default='cuda:0')
+parser.add_argument('--mode', default='kpcn')
+parser.add_argument('--input_channels', default=28, type=int)
+parser.add_argument('--hidden_channels', default=100, type=int)
+parser.add_argument('--num_layers', default=9, type=int)
+
+'''
+2. Preprocessing specifications
+eps
+'''
+parser.add_argument('--eps', default=0.00316, type=float)
+
+'''
+3. Training Specification
+val : should it perform validation
+early_stopping : should it perform early stopping
+trainset : dataset for training
+valset : dataset for validation
+lr : learning rate
+epoch : epoch
+criterion : which loss function should it use
+'''
+parser.set_defaults(do_val=False)
+parser.add_argument('--do_val', dest='do_val', action='store_true')
+parser.set_defaults(do_early_stopping=False)
+parser.add_argument('--do_early_stopping', dest='do_early_stopping', action='store_true')
+parser.add_argument('--lr', default=1e-4, type=float)
+parser.add_argument('--epochs', default=20, type=int)
+parser.add_argument('--loss', default='L1')
+
+'''
+4. More specifications
+kernel_size : for kpcn
+feat: add soon
+'''
+parser.add_argument('--kernel_size', default=5, type=int)
+
+
+
+def validation(diffuseNet, specularNet, dataloader, eps, criterion, device, mode='kpcn'):
   pass
   lossDiff = 0
   lossSpec = 0
@@ -34,7 +87,8 @@ def validation(diffuseNet, specularNet, dataloader, criterion, device, mode='KPC
     Y_diff = data['Reference'][:,:,:,:3].permute(permutation).to(device)
 
     outputDiff = diffuseNet(X_diff)
-    if mode == 'KPCN':
+    # if mode == 'KPCN':
+    if 'kpcn' in mode:
       X_input = crop_like(X_diff, outputDiff)
       outputDiff = apply_kernel(outputDiff, X_input, device)
 
@@ -45,7 +99,8 @@ def validation(diffuseNet, specularNet, dataloader, criterion, device, mode='KPC
     Y_spec = data['Reference'][:,:,:,3:6].permute(permutation).to(device)
     
     outputSpec = specularNet(X_spec)
-    if mode == 'KPCN':
+    # if mode == 'KPCN':
+    if 'kpcn' in mode:
       X_input = crop_like(X_spec, outputSpec)
       outputSpec = apply_kernel(outputSpec, X_input, device)
 
@@ -63,26 +118,32 @@ def validation(diffuseNet, specularNet, dataloader, criterion, device, mode='KPC
 
   return lossDiff/len(dataloader), lossSpec/len(dataloader), lossFinal/len(dataloader)
 
-def train(dataset, input_channels, device, feat=False, validDataSet=None, mode='KPCN', epochs=20, learning_rate=1e-4, show_images=False):
-  print('TRAINING WITH VALIDDATASET : {}'.format(validDataSet))
-  dataloader = torch.utils.data.DataLoader(dataset, batch_size=4,
+def train(mode, device, trainset, validset, eps, L, input_channels, hidden_channels, kernel_size, epochs, learning_rate, loss, do_early_stopping=False, show_images=False):
+  # print('TRAINING WITH VALIDDATASET : {}'.format(validset))
+  dataloader = torch.utils.data.DataLoader(trainset, batch_size=4,
                           shuffle=True, num_workers=4)
 
-  if validDataSet is not None:
-    validDataloader = torch.utils.data.DataLoader(validDataSet, batch_size=4, num_workers=4)
+  if validset is not None:
+    validDataloader = torch.utils.data.DataLoader(validset, batch_size=4, num_workers=4)
 
   # instantiate networks
-  if feat:
+  print(L, input_channels, hidden_channels, kernel_size, mode)
+  if 'feat' in mode:
     diffuseNet = make_feat_net(L, input_channels, hidden_channels, kernel_size, mode).to(device)
     specularNet = make_feat_net(L, input_channels, hidden_channels, kernel_size, mode).to(device)
   else:
+    print(mode)
     diffuseNet = make_net(L, input_channels, hidden_channels, kernel_size, mode).to(device)
     specularNet = make_net(L, input_channels, hidden_channels, kernel_size, mode).to(device)
 
   print(diffuseNet, "CUDA:", next(diffuseNet.parameters()).is_cuda)
   print(specularNet, "CUDA:", next(specularNet.parameters()).is_cuda)
-  
-  criterion = nn.L1Loss()
+
+  if loss == 'L1':
+    criterion = nn.L1Loss()
+  else:
+    print('Loss Not Supported')
+    return
 
   optimizerDiff = optim.Adam(diffuseNet.parameters(), lr=learning_rate)
   optimizerSpec = optim.Adam(specularNet.parameters(), lr=learning_rate)
@@ -113,20 +174,22 @@ def train(dataset, input_channels, device, feat=False, validDataSet=None, mode='
       # get the inputs
       X_diff = sample_batched['X_diff'].permute(permutation).to(device)
       Y_diff = sample_batched['Reference'][:,:,:,:3].permute(permutation).to(device)
-
+      # print(X_diff.shape, Y_diff.shape)
       # zero the parameter gradients
       optimizerDiff.zero_grad()
 
       # forward + backward + optimize
       outputDiff = diffuseNet(X_diff)
 
-      # print(outputDiff.shape)
+      # print()
 
-      if mode == 'KPCN':
+      # if mode == 'KPCN':
+      if 'kpcn' in mode:
         X_input = crop_like(X_diff, outputDiff)
         outputDiff = apply_kernel(outputDiff, X_input, device)
 
       Y_diff = crop_like(Y_diff, outputDiff)
+      # print(outputDiff.shape, Y_diff.shape)
 
       lossDiff = criterion(outputDiff, Y_diff)
       lossDiff.backward()
@@ -142,7 +205,8 @@ def train(dataset, input_channels, device, feat=False, validDataSet=None, mode='
       # forward + backward + optimize
       outputSpec = specularNet(X_spec)
 
-      if mode == 'KPCN':
+      # if mode == 'KPCN':
+      if 'kpcn' in mode:
         X_input = crop_like(X_spec, outputSpec)
         outputSpec = apply_kernel(outputSpec, X_input, device)
 
@@ -186,7 +250,7 @@ def train(dataset, input_channels, device, feat=False, validDataSet=None, mode='
       writer.add_scalar('specular loss', accuLossSpec if accuLossSpec != float('inf') else 0, epoch * len(dataloader) + i_batch)
       
     print('VALIDATION WORKING!')
-    validLossDiff, validLossSpec, validLossFinal = validation(diffuseNet, specularNet, validDataloader, criterion, device, mode)
+    validLossDiff, validLossSpec, validLossFinal = validation(diffuseNet, specularNet, validDataloader, eps, criterion, device, mode)
     writer.add_scalar('Valid total loss', np.log(np.abs(validLossFinal)) if accuLossFinal != float('inf') else 0, epoch * len(dataloader) + i_batch)
     writer.add_scalar('Valid diffuse loss', validLossDiff if accuLossDiff != float('inf') else 0, epoch * len(dataloader) + i_batch)
     writer.add_scalar('Valid specular loss', validLossSpec if accuLossSpec != float('inf') else 0, epoch * len(dataloader) + i_batch)
@@ -207,9 +271,9 @@ def train(dataset, input_channels, device, feat=False, validDataSet=None, mode='
     valLFinal.append(validLossFinal)
 
     total_epoch += 1
-    # if len(valLFinal) > 10 and valLFinal[-1] >= valLFinal[-2]:
-    #   print('EARLY STOPPING!')
-    #   break
+    if do_early_stopping and len(valLFinal) > 10 and valLFinal[-1] >= valLFinal[-2]:
+      print('EARLY STOPPING!')
+      break
     
     accuLossDiff = 0
     accuLossSpec = 0
@@ -222,89 +286,116 @@ def train(dataset, input_channels, device, feat=False, validDataSet=None, mode='
   return diffuseNet, specularNet, lDiff, lSpec, lFinal
 
 
-def unsqueeze_all(d):
-  for k, v in d.items():
-    d[k] = torch.unsqueeze(v, dim=0)
-  return d
-
-
-def denoise(diffuseNet, specularNet, data, device, debug=False):
-  with torch.no_grad():
-    out_channels = diffuseNet[len(diffuseNet)-1].out_channels
-    mode = 'DPCN' if out_channels == 3 else 'KPCN'
-    criterion = nn.L1Loss()
+def load_dataset(device, val=False):
+  # If already saved the data, load them
+  cropped = []
+  if not val:
+    for patch in os.listdir('data/'):
+      print('Loading patch : ' + patch)
+      cropped.append(torch.load('data/'+ patch))
+  else:
+    for patch in os.listdir('val/'):
+      print('Loading patch : ' + patch)
+      cropped.append(torch.load('val/'+ patch))
     
-    if debug:
-      print("Out channels:", out_channels)
-      print("Detected mode", mode)
-    
-    # make singleton batch
-    data = send_to_device(to_torch_tensors(data), device)
-    if len(data['X_diff'].size()) != 4:
-      data = unsqueeze_all(data)
-    
-    print(data['X_diff'].size())
-    
-    X_diff = data['X_diff'].permute(permutation).to(device)
-    Y_diff = data['Reference'][:,:,:,:3].permute(permutation).to(device)
+    # load to device and make dataset
+  cropped = to_torch_tensors(cropped)
+  cropped = send_to_device(cropped, device)
+  dataset = KPCNDataset(cropped)
+  return dataset
 
-    # forward + backward + optimize
-    outputDiff = diffuseNet(X_diff)
 
-    # print(outputDiff.shape)
+def train_dpcn(dataset, validset, device, eps, n_layers, size_kernel, in_channels, hidden_channels, epochs, lr, loss, do_early_stopping=False, save_dir=None):
+  pass
+  ddiffuseNet, dspecularNet, dlDiff, dlSpec, dlFinal = train('dpcn', device, dataset, validset, eps, n_layers, in_channels, hidden_channels, size_kernel, epochs, lr, loss, do_early_stopping)
+  torch.save(ddiffuseNet.state_dict(), 'trained_model/ddiffuseNet.pt')
+  torch.save(dspecularNet.state_dict(), 'trained_model/dspecularNet.pt')
+  with open('plot/dpcn.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(dlDiff)
+    writer.writerow(dlSpec)
+    writer.writerow(dlFinal)
+  return ddiffuseNet, dspecularNet, dlDiff, dlSpec, dlFinal
 
-    if mode == 'KPCN':
-      X_input = crop_like(X_diff, outputDiff)
-      outputDiff = apply_kernel(outputDiff, X_input, device)
 
-    Y_diff = crop_like(Y_diff, outputDiff)
 
-    lossDiff = criterion(outputDiff, Y_diff).item()
+def train_kpcn(dataset, validset, device, eps, n_layers, size_kernel, in_channels, hidden_channels, epochs, lr, loss, save_dir=None):
+  pass
+  kdiffuseNet, kspecularNet, klDiff, klSpec, klFinal = train('kpcn', device, dataset, validset, eps, n_layers, in_channels, hidden_channels, size_kernel, epochs, lr, loss)
+  torch.save(kdiffuseNet.state_dict(), 'trained_model/kdiffuseNet.pt')
+  torch.save(kspecularNet.state_dict(), 'trained_model/kspecularNet.pt')
+  with open('plot/kpcn.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(klDiff)
+    writer.writerow(klSpec)
+    writer.writerow(klFinal)
+  return kdiffuseNet, kspecularNet, klDiff, klSpec, klFinal
 
-    # get the inputs
-    X_spec = data['X_spec'].permute(permutation).to(device)
-    Y_spec = data['Reference'][:,:,:,3:6].permute(permutation).to(device)
 
-    # forward + backward + optimize
-    outputSpec = specularNet(X_spec)
+def train_feat_dpcn(dataset, validset, device, eps, n_layers, size_kernel, in_channels, hidden_channels, epochs, lr, loss, save_dir=None):
+  pass
+  ddiffuseNet, dspecularNet, dlDiff, dlSpec, dlFinal = train('feat_dpcn', device, dataset, validset, eps, n_layers, in_channels, hidden_channels, size_kernel, epochs, lr, loss)
+  torch.save(ddiffuseNet.state_dict(), 'trained_model/feat_ddiffuseNet.pt')
+  torch.save(dspecularNet.state_dict(), 'trained_model/feat_dspecularNet.pt')
+  with open('plot/feat_dpcn.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(dlDiff)
+    writer.writerow(dlSpec)
+    writer.writerow(dlFinal)
+  return ddiffuseNet, dspecularNet, dlDiff, dlSpec, dlFinal
 
-    if mode == 'KPCN':
-      X_input = crop_like(X_spec, outputSpec)
-      outputSpec = apply_kernel(outputSpec, X_input, device)
 
-    Y_spec = crop_like(Y_spec, outputSpec)
+def train_feat_kpcn(dataset, validset, device, eps, n_layers, size_kernel, in_channels, hidden_channels, epochs, lr, loss, save_dir=None):
+  pass
+  kdiffuseNet, kspecularNet, klDiff, klSpec, klFinal = train('feat_kpcn', device, dataset, validset, eps, n_layers, in_channels, hidden_channels, size_kernel, epochs, lr, loss)
+  torch.save(kdiffuseNet.state_dict(), 'trained_model/feat_kdiffuseNet.pt')
+  torch.save(kspecularNet.state_dict(), 'trained_model/feat_kspecularNet.pt')
+  with open('plot/feat_kpcn.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(klDiff)
+    writer.writerow(klSpec)
+    writer.writerow(klFinal)
+  return kdiffuseNet, kspecularNet, klDiff, klSpec, klFinal
 
-    lossSpec = criterion(outputSpec, Y_spec).item()
 
-    # calculate final ground truth error
-    albedo = data['origAlbedo'].permute(permutation).to(device)
-    albedo = crop_like(albedo, outputDiff)
-    outputFinal = outputDiff * (albedo + eps) + torch.exp(outputSpec) - 1.0
+def main():
+  args = parser.parse_args()
+  print(args)
+  
+  # Load Train & Validation Dataset
+  trainset = load_dataset(args.device)
+  validset = None
+  # print(args.do_val)
+  if args.do_val:
+    validset = load_dataset(args.device, args.do_val)
 
-    if debug:
-      writer = SummaryWriter('runs/results_'+mode)
-      print("Sample, denoised, gt")
-      sz = 15
-      orig = crop_like(data['finalInput'].permute(permutation), outputFinal)
-      orig = orig.cpu().permute([0, 2, 3, 1]).numpy()[0,:]
-      show_data(orig, figsize=(sz,sz), normalize=True)
-      print(orig.shape)
-      writer.add_image('noisy', np.transpose(orig, (1,2,0)))
-      img = outputFinal.cpu().permute([0, 2, 3, 1]).numpy()[0,:]
-      show_data(img, figsize=(sz,sz), normalize=True)
-      writer.add_image('denoised', img)
-      gt = crop_like(data['finalGt'].permute(permutation), outputFinal)
-      gt = gt.cpu().permute([0, 2, 3, 1]).numpy()[0,:]
-      show_data(gt, figsize=(sz,sz), normalize=True)
-      writer.add_image('clean', img)
+  assert(trainset[0]['X_diff'].shape[-1] == validset[0]['X_diff'].shape[-1])
+  input_channels = trainset[0]['X_diff'].shape[-1]
 
-    Y_final = data['finalGt'].permute(permutation).to(device)
+  diffuseNet, specularNet, Diff, Spec, Final = None, None, None, None, None
 
-    Y_final = crop_like(Y_final, outputFinal)
-    
-    lossFinal = criterion(outputFinal, Y_final).item()
-    
-    # if debug:
-    print("LossDiff:", lossDiff)
-    print("LossSpec:", lossSpec)
-    print("LossFinal:", lossFinal)
+  if args.mode == 'dpcn':
+    pass
+    diffuseNet, specularNet, Diff, Spec, Final = train_dpcn(trainset, validset, args.device, args.eps, args.num_layers, args.kernel_size, input_channels, args.hidden_channels, args.epochs, args.lr, args.loss)
+
+  elif args.mode == 'kpcn':
+    pass
+    diffuseNet, specularNet, Diff, Spec, Final = train_kpcn(trainset, validset, args.device, args.eps, args.num_layers, args.kernel_size, input_channels, args.hidden_channels, args.epochs, args.lr, args.loss)
+
+  elif args.mode == 'feat_dpcn':
+    pass
+    diffuseNet, specularNet, Diff, Spec, Final = train_feat_dpcn(trainset, validset, args.device, args.eps, args.num_layers, args.kernel_size, input_channels, args.hidden_channels, args.epochs, args.lr, args.loss)
+
+  elif args.mode == 'feat_kpcn':
+    pass
+    diffuseNet, specularNet, Diff, Spec, Final = train_feat_kpcn(trainset, validset, args.device, args.eps, args.num_layers, args.kernel_size, input_channels, args.hidden_channels, args.epochs, args.lr, args.loss)
+
+  else:
+    assert(False)
+
+  # plot_training(Diff, Spec, args.mode)
+  
+
+
+if __name__ == '__main__':
+  main()
