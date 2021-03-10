ee26e1a983b5361dd977b4b6f40b5bff581373bb
qbhan <smarthkb98@gmail.com>
2021-03-10 05:39:45 +0000

    FEATURE:Added more utility functions
    <description>
    
    <issue-number>

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import torch
 import sys
 import gc
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def to_torch_tensors(data):
@@ -65,3 +66,50 @@ def plot_training(diff, spec, filename):
   plt.legend()
   plt.savefig(filename + '.jpg')
   # plt.show()
+
+
+def ToneMap(c, limit=1.5):
+    # c: (W, H, C=3)
+    luminance = 0.2126 * c[:,:,0] + 0.7152 * c[:,:,1] + 0.0722 * c[:,:,2]
+    col = c.copy()
+    col[:,:,0] /=  (1.0 + luminance / limit)
+    col[:,:,1] /=  (1.0 + luminance / limit)
+    col[:,:,2] /=  (1.0 + luminance / limit)
+    return col
+
+def LinearToSrgb(c):
+    # c: (W, H, C=3)
+    kInvGamma = 1.0 / 2.2
+    return np.clip(c ** kInvGamma, 0.0, 1.0)
+
+def ToneMapBatch(c):
+    # c: (B, C=3, W, H)
+    luminance = 0.2126 * c[:,0,:,:] + 0.7152 * c[:,1,:,:] + 0.0722 * c[:,2,:,:]
+    col = c.copy()
+    col[:,0,:,:] /= (1 + luminance / 1.5)
+    col[:,1,:,:] /= (1 + luminance / 1.5)
+    col[:,2,:,:] /= (1 + luminance / 1.5)
+    col = np.clip(col, 0, None)
+    kInvGamma = 1.0 / 2.2
+    return np.clip(col ** kInvGamma, 0.0, 1.0)
+
+
+def crop_like(src, tgt):
+    src_sz = np.array(src.shape)
+    tgt_sz = np.array(tgt.shape)
+
+    # Assumes the spatial dimensions are the last two
+    # delta = (src_sz[2:4]-tgt_sz[2:4])
+    delta = (src_sz[-2:]-tgt_sz[-2:])
+    crop = np.maximum(delta // 2, 0)  # no negative crop
+    crop2 = delta - crop
+
+    if (crop > 0).any() or (crop2 > 0).any():
+        # NOTE: convert to ints to enable static slicing in ONNX conversion
+        src_sz = [int(x) for x in src_sz]
+        crop = [int(x) for x in crop]
+        crop2 = [int(x) for x in crop2]
+        return src[..., crop[0]:src_sz[-2]-crop2[0],
+                   crop[1]:src_sz[-1]-crop2[1]]
+    else:
+        return src
